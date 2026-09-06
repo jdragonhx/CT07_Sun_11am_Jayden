7dacef2d710fc3913b1796b39bd521730e38649c
jdragonhx <jayden.dhx@gmail.com>
2026-09-06 04:45:24 +0000

    summition

--- testquestion1.js
+++ testquestion1.js
@@ -1,1 +1,17 @@
+let balls;
 
+function setup() {
+    createCanvas(600, 400);
+}
+
+function draw() {
+    background(150);
+    if(mouse.presses()) {
+        balls = new Sprite(mouse.x,mouse.y,30);
+        fill(random(0, 255),random(0, 255),random(0, 255));
+        balls = "dynamic";
+        world.gravity.y = 5
+        
+    }
+
+}
--- testquestion2.js
+++ testquestion2.js
@@ -1,1 +1,21 @@
+let balls;
+let floor;
 
+function setup() {
+    createCanvas(600, 400);
+    floor = new Sprite(300, 400, 600, 20);
+    floor.collider = "static";
+}
+
+function draw() {
+    background(150);
+    if(mouse.presses()) {
+        balls = new Sprite(mouse.x,mouse.y,30);
+        fill(random(0, 255),random(0, 255),random(0, 255));
+        balls.collider = "dynamic";
+        world.gravity.y = 5;
+        balls.bounciness = 0.5;
+        
+    }
+
+}
