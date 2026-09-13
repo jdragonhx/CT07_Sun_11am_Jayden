209ffede219ace46e5f0d6876e297fca2360b7ab
jdragonhx <jayden.dhx@gmail.com>
2026-09-13 03:32:32 +0000

    Sep 13, 2026, 3:32 AM

--- testquestion2.js
+++ testquestion2.js
@@ -1,12 +1,10 @@
 let balls;
 let floor;
-
 function setup() {
     createCanvas(600, 400);
     floor = new Sprite(300, 400, 600, 20);
     floor.collider = "static";
 }
-
 function draw() {
     background(150);
     if(mouse.presses()) {
@@ -15,7 +13,6 @@ function draw() {
         balls.collider = "dynamic";
         world.gravity.y = 5;
         balls.bounciness = 0.5;
-        
     }
 
 }
